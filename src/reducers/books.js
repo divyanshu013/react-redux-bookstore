53b12cf1efa275ddfb4040235025a441b5e4a589
Divyanshu Maithani <div.blackcat@gmail.com>
2017-05-29 22:32:08 +0530

    Update books reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,11 +1,15 @@
-import { ADD_BOOK, FETCH_BOOKS_SUCCESS } from '../actions';
+import { ADD_BOOK_SUCCESS, UPDATE_BOOK_SUCCESS, FETCH_BOOKS_SUCCESS } from '../actions';
 
 export default function books(state = [], action) {
   switch (action.type) {
-    case ADD_BOOK:
+    case ADD_BOOK_SUCCESS:
       return [
         action.book,
         ...state
+      ];
+    case UPDATE_BOOK_SUCCESS:
+      return [
+        ...state.map(book => (book.id !== action.book.id ? book : action.book))
       ];
     case FETCH_BOOKS_SUCCESS:
       return action.books;
