2b79d61500d96283066a25011750596645befcfc
Divyanshu Maithani <div.blackcat@gmail.com>
2017-06-02 23:56:18 +0530

    Tweak UpdateBookForm container

--- src/containers/BookFormContainers/UpdateBookForm.js
+++ src/containers/BookFormContainers/UpdateBookForm.js
@@ -1,11 +1,63 @@
+import React from 'react';
 import { connect } from 'react-redux';
 
 import BookForm from '../../components/Books/BookForm';
+import { saveBook } from '../../actions';
 
 const mapStateToProps = state => ({
   authors: state.authors
 });
 
-const UpdateBookForm = connect(mapStateToProps)(BookForm);
+const mapDispatchToProps = dispatch => ({
+  saveBook: book => dispatch(saveBook(book))
+});
+
+class UpdateBookFormComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      book: {
+        title: '',
+        id: '',
+        authorId: '',
+        image: '',
+        rating: 0,
+        category: ''
+      }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event, data) {
+    const value = data.value || data.rating;
+    const name = data.name;
+
+    const book = { ...this.state.book };
+    book[name] = value;
+
+    this.setState({
+      book
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.saveBook(this.state.book);
+  }
+
+  render() {
+    return (
+      <BookForm
+        book={this.state.book} handleChange={this.handleChange} handleSubmit={this.handleSubmit}
+        authors={this.props.authors}
+      />
+    );
+  }
+}
+
+const UpdateBookForm = connect(mapStateToProps, mapDispatchToProps)(UpdateBookFormComponent);
 
 export default UpdateBookForm;
